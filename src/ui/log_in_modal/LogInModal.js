45bcd3cba469f60835847a7d1346f6d530092503
Madina <dimasempaj@gmail.com>
2023-10-31 21:28:09 +0600

    Working on user authorization. Added error output when user data is entered incorrectly.

--- src/ui/log_in_modal/LogInModal.js
+++ src/ui/log_in_modal/LogInModal.js
@@ -4,13 +4,36 @@ import axios from "axios";
 import {useState} from "react";
 
 function LogInModal({visible, close}) {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [errors, setErrors] = useState('')
+
+    function logIn() {
+        axios.post('https://frost.runtime.kz/api/auth/token', {
+            username: email,
+            password: password,
+        })
+            .then(() => {
+                setErrors('');
+            })
+            .catch(error => {
+                let response = error.response;
+                console.log(response.data)
+                if (response.status === 400) {
+                    setErrors(response.data.message)
+                }
+            })
+    }
+
     return (
         <Modal visible={visible} close={close}>
             <h2>Вход в учетную запись</h2>
-            <input placeholder="Адрес электронной почты" type="text"/>
-            <input placeholder="Пароль" type="password"/>
+            <span className="error">{errors}</span>
+            <input placeholder="Адрес электронной почты" type="text" value={email} onChange={(event) => setEmail(event.target.value)}/>
+            <input placeholder="Пароль" type="password" value={password} onChange={(event) => setPassword(event.target.value)}/>
             <a href="#">Забыли пароль?</a>
-            <Button text={"Войти"} buttonStyle={buttonStyles.normal}/>
+            <Button text={"Войти"} buttonStyle={buttonStyles.normal} onClick={logIn}/>
         </Modal>
     );
 }
--- src/ui/registration_modal/Registration.js
+++ src/ui/registration_modal/Registration.js
@@ -2,6 +2,7 @@ import Button, {buttonStyles} from "../button/Button";
 import Modal from "../modal/Modal";
 import {useState} from "react";
 import axios from "axios";
+import './Registration.css'
 
 function Registration({visible, close}) {
     const [firstName, setFirstName] = useState('');
@@ -9,19 +10,36 @@ function Registration({visible, close}) {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
+    const [errors, setErrors] = useState({
+        firstNameError: '',
+        lastNameError: '',
+        emailError: '',
+        passwordError: '',
+    })
+
     function userRegistration() {
-        alert('abc');
         axios.post('https://frost.runtime.kz/api/registration', {
                 first_name: firstName,
                 last_name: lastName,
                 email: email,
                 password: password,
         }).then(() => {
-            //
+            setErrors(function () {
+                return {
+                    emailError: ''
+                }
+            })
         }).catch(error => {
             let response = error.response;
             if (response.status === 400) {
-                console.log(response.data);
+                setErrors(function () {
+                    return {
+                        firstNameError: response.data.errors.first_name,
+                        lastNameError: response.data.errors.last_name,
+                        emailError: response.data.errors.email,
+                        passwordError: response.data.errors.password,
+                    }
+                })
             }
         });
     }
@@ -29,11 +47,21 @@ function Registration({visible, close}) {
     return(
         <Modal visible={visible} close={close}>
             <h2>Создание учетной записи</h2>
+            <div className="errors">
+                <span>{errors.firstNameError}</span>
+                <span>{errors.lastNameError}</span>
+            </div>
             <div className="user_name">
                 <input placeholder="Имя" type="text" value={firstName} onChange={(event) => setFirstName(event.target.value)}/>
                 <input placeholder="Фамилия" value={lastName} onChange={(event) => setLastname(event.target.value)}/>
             </div>
+            <div className="errors">
+                <span>{errors.emailError}</span>
+            </div>
             <input placeholder="Адрес электронной почты" value={email} onChange={(event) => setEmail(event.target.value)}/>
+            <div className="errors">
+                <span>{errors.passwordError}</span>
+            </div>
             <input placeholder="Пароль" type="password" value={password} onChange={(event) => setPassword(event.target.value)}/>
             <input placeholder="Повторите пароль" type="password"/>
             <Button text={"Зарегистрироваться"} buttonStyle={buttonStyles.normal} onClick={userRegistration}/>
