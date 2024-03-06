be35832997eaa10710ea87345b0692af9e669767
Madina <dimasempaj@gmail.com>
2024-03-07 01:48:14 +0600

    Working on filters

--- src/features/filters/filtersAPI.js
+++ src/features/filters/filtersAPI.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
-export const getProducts = async (values) => {
-    let response = await axios.get('https://frost.runtime.kz/api/products', {
+export const getProducts = async (page, values) => {
+    let response = await axios.get('/products', {
         params: {
-            page: 1,
+            page: page,
             size: 6,
             brandId: values.brandId,
             modelsId: values.modelId,
             generationId: values.generationId,
-            available: values.available
+            available: values.available,
         }
     })
     return response.data;
@@ -16,5 +16,23 @@ export const getProducts = async (values) => {
 
 export const getBrands = async () => {
     let response = await axios.get('/brands');
+    return response.data;
+}
+
+export const getModels = async (brand) => {
+    let response = await axios.get('/models', {
+        params: {
+            brandId: brand,
+        }
+    });
+    return response.data;
+}
+
+export const getGenerations = async (model) => {
+    let response = await axios.get('/generations', {
+        params: {
+            modelId: model
+        }
+    });
     return response.data;
 }
--- src/features/filters/filtersSlice.js
+++ src/features/filters/filtersSlice.js
@@ -1,35 +1,78 @@
 import {createSlice} from "@reduxjs/toolkit";
-import {getBrands} from "./filtersAPI";
+import {getBrands, getGenerations, getModels, getProducts} from "./filtersAPI";
 
 const initialState = {
-    brands: [],
-    models: [],
-    generations: [],
-    selectedBrand: undefined,
-    selectedModel: undefined,
-    selectedGeneration: undefined,
+    productPage: [],
+    page: {},
+    brands: [{text: 'Все марки', value: null}],
+    models: [{text: 'Все модели', value: null}],
+    generations: [{text: 'Все поколения', value: null}],
+    filter: {
+        selectedBrand: undefined,
+        selectedModel: undefined,
+        selectedGeneration: undefined,
+        available: 0
+    },
 };
 
 const filtersSlice = createSlice({
-    name: 'categories',
+    name: 'filter',
     initialState,
     reducers: {
-
+        setProducts: (state, action) => {
+            state.productsPage = action.payload;
+        },
+        setPageState: (state, action) => {
+            state.pages = action.payload;
+        },
+        setBrands: (state, action) => {
+            state.brands = action.payload;
+        },
+        setModels: (state, action) => {
+            state.models = action.payload;
+        },
+        setGenerations: (state, action) => {
+            state.generations = action.payload;
+        },
+        setFilter: (state, action) => {
+            state.filter = action.payload;
+        }
     }
 });
 
-export const loadBrands = () => dispatch => {
-    getBrands().then((brands) => {
-
-    })
+export const PageChange = (page, filter) => (dispatch) => {
+    getProducts(page, filter)
+        .then(response => {
+            dispatch(setProducts(response.items));
+            dispatch(setPageState(response));
+        });
 };
 
-export const selectBrand = brandId => dispatch => {
-    //
+export const loadBrands = () => (dispatch, getState) => {
+    const state = getState();
+    getBrands()
+        .then((brands => {
+            dispatch(setBrands([...state.filter.brands,
+                ...brands.map(item => ({text: item.name, value: item.id}))]))
+        }))
+};
+
+let models = [{text: 'Все модели', value: null}];
+let generations = [{text: 'Все поколения', value: null}];
+
+export const loadModels = brandId => dispatch => {
+    if (brandId === null) {
+        dispatch(setModels(''));
+    } else {
+        getModels(brandId)
+            .then(modelsAPI => {
+                models.push(...modelsAPI.map(item => ({text: item.name, value: item.id})))
+                dispatch(setModels(models));
+            })
+    }
 };
 
 
-
-export const {  } = filtersSlice.actions;
+export const {setProducts, setPageState, setBrands, setModels} = filtersSlice.actions;
 
 export default filtersSlice.reducer;
